6051433264a1399ef612995bca2f4f5f0745b125
Frijolito69 <143144982+Frijolito69@users.noreply.github.com>
2023-09-08 01:57:28 +0000

    Resultados clase 7 septiembre

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<"Juego de mascotas"<<std::endl;
+    std::cout << "Juego de mascotas" << std::endl;
 
     Mascota m1("Heidy");
     m1.jugar();
     m1.jugar();
     m1.jugar();
 
-    std::cout<<m1.DecirNombre()<< "tiene"
-    <<m1.DecirHambre()<< "de hambre.";
+    std::cout << m1.DecirNombre() << "tiene"
+              << m1.DecirHambre() << "de hambre.";
 
     m1.comer(5);
 
