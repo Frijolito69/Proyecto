56b908b0b72fd07c0ae1a0af860bf3f91252aadb
Frijolito69 <143144982+Frijolito69@users.noreply.github.com>
2023-09-08 01:54:49 +0000

    Resultados clase 7 septiembre

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Mascota.hpp>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"Juego de mascotas"<<std::endl;
+
+    Mascota m1("Heidy");
+    m1.jugar();
+    m1.jugar();
+    m1.jugar();
+
+    std::cout<<m1.DecirNombre()<< "tiene"
+    <<m1.DecirHambre()<< "de hambre.";
+
+    m1.comer(5);
+
+    return 0;
+}
